8a816a1cf840f0a477d1f8bd582981ae09172f55
diego <dgiraldez@gmail.com>
2013-12-03 20:12:50 -0300

    Almacenamiento de historicos

--- capture.php
+++ capture.php
@@ -61,5 +61,49 @@ if ($row["enabled"]) {
 $update = "UPDATE input SET current = ".$value.", last = '".$datetime."' WHERE id=".$id;
 mysqli_query($con,$update) or die('Error: ' . mysqli_error($con));
 
+//actualizo historico
+
+//scale 0 agrupa por hora, trunco minuto y segundo
+$dateh = date_create_from_format('dmyHis', $date.$time)->format('Y-m-d H:00:00');
+$where=" WHERE inputId = '".$id."' and date = '".$dateh."' and scale = 0";
+$query="SELECT min, max FROM history".$where;
+$result = mysqli_query($con,$query);
+if (!$result) die('Error: ' . mysqli_error($con));
+
+if (mysqli_num_rows($result) == 1) { //update
+	$row = mysqli_fetch_assoc($result);
+	if ($value > $row["max"]) $row["max"] = $value;
+	if ($value < $row["min"]) $row["min"] = $value;
+	//asumo que las muestras llegan en orden, por eso la ultima siempre es close
+	$update = "UPDATE history SET min = ".$row["min"].", max = ".$row["max"].", close = ".$value.$where;
+	mysqli_query($con,$update) or die('Error: ' . mysqli_error($con));
+	
+} elseif (mysqli_num_rows($result) == 0) { //inserto
+	$insert="INSERT INTO history (inputId, date, open, min, max, close, scale) VALUES ($id,'$dateh',$value,$value,$value,$value,0)";
+	mysqli_query($con,$insert) or die('Error: ' . mysqli_error($con));
+	
+} else die('Error: filas distinto de 1');
+
+//scale 1 agrupa por dia, trunco hora, minuto y segundo
+$datey = date_create_from_format('dmyHis', $date.$time)->format('Y-m-d 00:00:00');
+$where=" WHERE inputId = '".$id."' and date = '".$datey."' and scale = 1";
+$query="SELECT min, max FROM history".$where;
+$result = mysqli_query($con,$query);
+if (!$result) die('Error: ' . mysqli_error($con));
+
+if (mysqli_num_rows($result) == 1) { //update
+	$row = mysqli_fetch_assoc($result);
+	if ($value < $row["min"]) $row["min"] = $value;
+	if ($value > $row["max"]) $row["max"] = $value;
+	//asumo que las muestras llegan en orden, por eso la ultima siempre es close
+	$update = "UPDATE history SET min = ".$row["min"].", max = ".$row["max"].", close = ".$value.$where;
+	mysqli_query($con,$update) or die('Error: ' . mysqli_error($con));
+	
+} elseif (mysqli_num_rows($result) == 0) { //inserto
+	$insert="INSERT INTO history (inputId, date, open, min, max, close, scale) VALUES ($id,'$datey',$value,$value,$value,$value,1)";
+	mysqli_query($con,$insert) or die('Error: ' . mysqli_error($con));
+	
+} else die('Error: filas distinto de 1');
+
 mysqli_close($con);
 ?>
